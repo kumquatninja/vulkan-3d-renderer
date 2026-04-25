437e501a4deb9d3bea0c4d3797b8aa49b4f82001
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2026-04-25 15:57:21 -0700

    Created Windows batch file for compilation. Removed Vim swap file from repo.

---
+++ compile.bat
@@ -0,0 +1,7 @@
+cl /EHsc /std:c++17 main.cpp ^
+   /I"external\glfw-3.4.bin.WIN64\include" ^
+   /I"%VULKAN_SDK%\Include" ^
+   /link ^
+   /LIBPATH:"external\glfw-3.4.bin.WIN64\lib-vc2022" ^
+   /LIBPATH:"%VULKAN_SDK%\Lib" ^
+   glfw3.lib vulkan-1.lib user32.lib gdi32.lib shell32.lib msvcrt.lib
