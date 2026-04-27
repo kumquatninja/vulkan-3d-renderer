19fc359dffe045eeb85624b40fd30af3d9b30653
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2026-04-27 10:17:36 -0700

    Freed texture image memory during cleanup.

--- main.cpp
+++ main.cpp
@@ -517,6 +517,9 @@ private:
 	void cleanup() {
 		cleanupSwapChain();
 
+		vkDestroyImage(device, textureImage, nullptr);
+		vkFreeMemory(device, textureImageMemory, nullptr);
+
 		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
 			vkDestroyBuffer(device, uniformBuffers[i], nullptr);
 			vkFreeMemory(device, uniformBuffersMemory[i], nullptr);
