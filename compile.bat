57d0c775cb113f3e39853e2e097b95a0c89a567e
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2026-05-02 20:27:14 -0700

    Updated compile batch file. Deprecated.

--- compile.bat
+++ compile.bat
@@ -1,10 +1,11 @@
-cl /EHsc /Zi /O2 /std:c++17 main.cpp WindowManager.cpp ^
-   /I"external\glfw-3.4.bin.WIN64\include" ^
-   /I"external\glm" ^
-   /I"external\stb" ^
-   /I"external\tinyobjloader" ^
+cl /EHsc /Zi /O2 /std:c++17 src/main.cpp src/WindowManager.cpp src/Renderer.cpp ^
+   /I"include\" ^
+   /I"third_party\glfw-3.4.bin.WIN64\include" ^
+   /I"third_party\glm" ^
+   /I"third_party\stb" ^
+   /I"third_party\tinyobjloader" ^
    /I"%VULKAN_SDK%\Include" ^
    /link ^
-   /LIBPATH:"external\glfw-3.4.bin.WIN64\lib-vc2022" ^
+   /LIBPATH:"third_party\glfw-3.4.bin.WIN64\lib-vc2022" ^
    /LIBPATH:"%VULKAN_SDK%\Lib" ^
    glfw3.lib vulkan-1.lib user32.lib gdi32.lib shell32.lib msvcrt.lib
