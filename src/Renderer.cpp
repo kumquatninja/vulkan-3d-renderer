0350122735b769971091c6befbe4051ee62e96b2
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2026-05-02 22:45:44 -0700

    Added rotation of model back into uniform buffer.

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1317,7 +1317,8 @@ namespace KQ {
 		float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
 		UniformBufferObject ubo{};
-		ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // need to rotate the viking house
+		ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		ubo.model *= glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 		ubo.view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
 		ubo.proj[1][1] *= -1; // GLM was originally designed for OpenGL, where the Y component of the clip coordinates is inverted.
