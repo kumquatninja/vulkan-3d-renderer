8ebaba46dc74120b97c778777c8cfcc2751ed506
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2026-05-08 22:32:03 -0700

    Created GameObject struct type.

---
+++ include/GameObject.hpp
@@ -0,0 +1,42 @@
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_FORCE_DEFAULTS_ALIGNED_GENTYPES
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <string>
+#include <vector>
+
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#   include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
+
+namespace KQ {
+    struct GameObject {
+        glm::vec3 position = glm::vec3(0.0f);
+        glm::vec3 rotation = glm::vec3(0.0f);
+        glm::vec3 scale = glm::vec3(1.0f);
+        
+        std::vector<vk::raii::Buffer> uniformBuffers;
+        std::vector<vk::raii::DeviceMemory> uniformBuffersMemory;
+        std::vector<void*> uniformBuffersMapped;
+
+        std::vector<vk::raii::DescriptorSet> descriptorSets;
+
+        glm::mat4 getModelMatrix() const {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, position);
+            model = glm::rotate(model, rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+            model = glm::rotate(model, rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::rotate(model, rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+            model = glm::scale(model, scale);
+            return model;
+        }
+        // std::string modelPath = "assets/models/viking_room.obj";
+    };
+}
