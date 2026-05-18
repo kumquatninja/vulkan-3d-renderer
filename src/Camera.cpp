2b4c3f187d018909a1ea5a583541d0b90e514c59
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2026-05-18 11:24:58 -0700

    Corrected camera vector calculations.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -6,13 +6,11 @@ namespace KQ
     }
 
     void Camera::UpdateCameraVectors() {
-        glm::vec3 offset;
-        offset.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch)) * m_Distance;
-        offset.y = sin(glm::radians(m_Pitch)) * m_Distance;
-        offset.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch)) * m_Distance;
+        m_Forward.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+        m_Forward.y = sin(glm::radians(m_Pitch));
+        m_Forward.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
 
-        m_Pos = m_TargetPos + offset;
-        m_Forward = glm::normalize(m_TargetPos - m_Pos);
+        m_Forward = glm::normalize(m_Forward);
         m_Right = glm::normalize(glm::cross(m_Forward, m_WorldUp));
         m_Up = glm::normalize(glm::cross(m_Right, m_Forward));
     }
